4de390cfe6690190904f97966a6dec56e1078f9a
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-07-25 16:30:57 -0600

    project finished.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -42,6 +42,9 @@ class Instructor extends Person {
         grade(student, subject){
             return `${student} receives a perfect score on ${subject}.`
         }
+        changeGrade(student){
+            return student.grade + (Math.random() < 0.5 ? -1 : 1) * Math.floor((Math.random()*50)+1);
+        }
 }
 
 // #### Student
@@ -62,7 +65,8 @@ class Student extends Person {
         super(Sattributes);
         this.previousBackground = Sattributes.previousBackground;
         this.className = Sattributes.className;
-        this.favSubjects = Sattributes.favSubjects; //This needs to be an array.
+        this.favSubjects = Sattributes.favSubjects;
+        this.grade = Sattributes.grade;
     }
         listsSubjects(){
             return `${this.name}'s favorite subjects are  ${this.favSubjects}.`
@@ -72,6 +76,13 @@ class Student extends Person {
         }
         sprintChallenge(subject){
             return `${this.name} has begun sprint challenge on ${subject}.`
+        }
+        graduate(student){
+            if (student.grade >70){
+                console.log(`Congrats ${this.name}, you graduated!`)
+            } else {
+                console.log(`Sorry, ${this.name}, better luck next time!`)
+            }
         }
 }
 
@@ -135,7 +146,8 @@ const barry = new Student({
     location: 'San Francisco',
     previousBackground: 'Baseball Player',
     className: 'Web22',
-    favSubjects: ['baseball', 'statistics','Web Development']
+    favSubjects: ['baseball', 'statistics','Web Development'],
+    grade: 65
 });
 
 const bonds = new Student({
@@ -144,7 +156,8 @@ const bonds = new Student({
     location: 'Florida',
     previousBackground: 'Lawyer',
     className: 'iOS10',
-    favSubjects: ['math', 'science', 'Biology']
+    favSubjects: ['math', 'science', 'Biology'],
+    grade: 70
 });
 
 const joe = new Student({
@@ -153,7 +166,8 @@ const joe = new Student({
     location: 'Minnesota',
     previousBackground: 'Garbage man',
     className: 'Data Science 5',
-    favSubjects: ['history', 'music', 'art']
+    favSubjects: ['history', 'music', 'art'],
+    grade: 75
 });
 
 const madison = new ProjectManager({
@@ -201,3 +215,14 @@ console.log(madison.standUp('Cohort 99'));
 console.log(buster.debugsCode(joe.name, 'VS code'));
 console.log(jordan.speak());
 
+console.log(griffey.changeGrade(barry));
+console.log(barry.graduate(barry));
+
+
+// #### Stretch Problem
+
+// * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+// * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
+// * Add a graduate method to a student.
+//   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
+//   * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
